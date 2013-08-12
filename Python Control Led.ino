aa382ca2f464565cfb43ec354968f560cdf9869d
Moonshiness554 <sasha.jaffarove@gmail.com>
2013-08-12 11:04:58 -0700

    Update and rename README.md to Python Control Led.ino

---
+++ Python Control Led.ino
@@ -0,0 +1,30 @@
+Led-Control
+===========
+
+void setup()
+{
+  pinMode(13, OUTPUT);
+  Serial.begin(9600);
+  
+}
+
+void loop()
+{
+    if(Serial.available())
+    {
+      int theInput = Serial.parseInt();
+      
+      if(theInput == 9)
+      {
+        digitalWrite(13,HIGH);
+      }
+      
+      if ( theInput == 6)
+      {
+        digitalWrite(13, LOW);
+      }
+    
+      
+    }
+    delay(100);
+}
